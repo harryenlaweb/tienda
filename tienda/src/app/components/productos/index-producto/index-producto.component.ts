2d0da5a8a6026916ed7073e8567f2d59b98aec0f
harryenlaweb <harryenlaweb@gmail.com>
2021-11-06 19:28:36 -0300

    clase40

--- tienda/src/app/components/productos/index-producto/index-producto.component.ts
+++ tienda/src/app/components/productos/index-producto/index-producto.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+declare var noUiSlider;
+declare var jQuery:any;
+declare var $:any;
 
 @Component({
   selector: 'app-index-producto',
@@ -10,6 +13,30 @@ export class IndexProductoComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+
+    var slider : any = document.getElementById('slider');
+    noUiSlider.create(slider, {
+        start: [0, 1000],
+        connect: true,        
+        range: {
+            'min': 0,
+            'max': 1000
+        },
+        tooltips: [true,true],
+        pips: {
+          mode: 'count', 
+          values: 5,
+          
+        }
+    })
+
+    slider.noUiSlider.on('update', function (values) {
+      console.log(values);
+      
+        $('.cs-range-slider-value-min').val(values[0]);
+        $('.cs-range-slider-value-max').val(values[1]);
+    });
+    $('.noUi-tooltip').css('font-size','11px');
   }
 
 }
