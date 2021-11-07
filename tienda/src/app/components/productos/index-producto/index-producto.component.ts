dc46f54434fc26b03e0af888654150bb17cc6652
harryenlaweb <harryenlaweb@gmail.com>
2021-11-07 11:48:24 -0300

    clase45

--- tienda/src/app/components/productos/index-producto/index-producto.component.ts
+++ tienda/src/app/components/productos/index-producto/index-producto.component.ts
@@ -15,6 +15,7 @@ export class IndexProductoComponent implements OnInit {
   public filter_categoria = '';
   public productos : Array<any> = [];
   public filter_producto = '';
+  public filter_cat_productos = 'todos';
   public url;
   public load_data = true;  
 
@@ -98,8 +99,26 @@ export class IndexProductoComponent implements OnInit {
           return item.precio >= min && item.precio <=max
         }))
       }
-    )
-    
+    )    
+  }
+
+  buscar_por_categoria(){
+    if(this.filter_cat_productos == 'todos'){
+      this._clienteService.listar_productos_publico(this.filter_producto).subscribe(
+        response=>{
+          this.productos = response.data;
+          this.load_data = false;
+        }
+      )
+    }else{
+      this._clienteService.listar_productos_publico(this.filter_producto).subscribe(
+        response=>{
+          this.productos = response.data;
+          this.productos = this.productos.filter(item=>item.categoria == this.filter_cat_productos);
+        }
+      )
+      
+    }
     
   }
 
