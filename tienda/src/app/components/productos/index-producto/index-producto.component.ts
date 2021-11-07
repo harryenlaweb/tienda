50664731a436fe2743145b13d1cd70995d9840dd
harryenlaweb <harryenlaweb@gmail.com>
2021-11-07 17:10:41 -0300

    clase47

--- tienda/src/app/components/productos/index-producto/index-producto.component.ts
+++ tienda/src/app/components/productos/index-producto/index-producto.component.ts
@@ -35,7 +35,7 @@ export class IndexProductoComponent implements OnInit {
       params=>{
         this.route_categoria = params['categoria'];
         if(this.route_categoria){
-          this._clienteService.listar_productos_publico(this.filter_producto).subscribe(
+          this._clienteService.listar_productos_publico('').subscribe(
             response => {
               this.productos = response.data;
               this.productos = this.productos.filter(item=>item.categoria.toLowerCase() == this.route_categoria);
@@ -44,7 +44,7 @@ export class IndexProductoComponent implements OnInit {
           )
           
         }else{
-          this._clienteService.listar_productos_publico(this.filter_producto).subscribe(
+          this._clienteService.listar_productos_publico('').subscribe(
             response => {
               this.productos = response.data;
               this.load_data = false;
@@ -145,6 +145,16 @@ export class IndexProductoComponent implements OnInit {
       
     }
     
+  }
+
+  reset_productos(){
+    this.filter_producto = '';
+    this._clienteService.listar_productos_publico('').subscribe(
+      response=>{
+        this.productos = response.data;
+        this.load_data = false;
+      }
+    )
   }
 
 }
